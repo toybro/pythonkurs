d6a1019a90e09a537a34c3206b09e78459471ed0
toybro <34341513+toybro@users.noreply.github.com>
2025-10-02 20:44:37 +0200

    Add files via upload

---
+++ Projekt1/lekcja_4_3.py
@@ -0,0 +1,8 @@
+lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for liczba in lista_liczb:
+    if liczba % 2 == 0:
+        continue
+    print("Liczba jest nieparzysta", liczba)
+
+print("Koniec pętli 1.")
