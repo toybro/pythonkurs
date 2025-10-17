3d46ab3bf0d5fc7a43946a948b60d68dc62b4a73
toybro <krzysiupoc@gmail.com>
2025-10-17 22:20:37 +0200

    Add files via upload

---
+++ ZADANIA DOMOWE/Praca domowa 1.2 Wprowadzenie do Pythona.py
@@ -0,0 +1,47 @@
+print("Zadanie 1")
+
+print("Podaj 3 cyfry liczby.")
+cyfra1 = int(input("Podaj cyfre 1: "))
+cyfra2 = int(input("Podaj cyfre 2: "))
+cyfra3 = int(input("Podaj cyfre 3: "))
+
+print(f"Podałeś liczby {cyfra1}, {cyfra2}, {cyfra3}")
+suma = cyfra1 + cyfra2 + cyfra3
+
+print(f"Suma cyfry liczby = {suma}")
+
+iloczyn = cyfra1 * cyfra2 * cyfra3
+print(f"Iloczyn cyfry liczby = {iloczyn}")
+print("*"*50)
+
+print("Zadanie 2")
+
+wynagrodzenie = int(input("Podaj wynagrodzenie: "))
+rata_kredytu = int(input("Podaj rate kredytu: "))
+oplata_komunalna = int(input("Podaj wysokosc oplaty komunalnej: "))
+kwota_pozostala_do_dyspozycji = wynagrodzenie-(rata_kredytu+oplata_komunalna)
+print(f"Twoja pozostala kwota do dyspozycji w miesiacu wynosi {kwota_pozostala_do_dyspozycji}")
+
+print("*"*50)
+
+print("Zadanie 3")
+d1 = int(input("Podaj długość pierwszej przekątnej: "))
+d2 = int(input("Podaj długość drugiej przekątnej: "))
+pole = (d1 * d2) / 2
+print("Pole powierzchni rombu wynosi:", pole)
+
+print("*"*50)
+
+print("Zadanie 4")
+print("To be")
+print("or not")
+print("to be")
+
+print("*"*50)
+
+print("Zadanie 5")
+print("\t\t\t\tLife is what happens")
+print("\t\t\twhen")
+print("\t\tyou're busy making other plans")
+print("\tJohn Lennon.")
+
