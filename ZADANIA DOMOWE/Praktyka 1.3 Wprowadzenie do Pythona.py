6401cdb9d6d670ee1293a2dd8221a710bd6ad998
toybro <krzysiupoc@gmail.com>
2025-10-17 22:59:00 +0200

    Add files via upload

---
+++ ZADANIA DOMOWE/Praktyka 1.3 Wprowadzenie do Pythona.py
@@ -0,0 +1,28 @@
+print("Zadanie 1")
+liczba = input("Podaj dwucyfrową liczbę: ")
+print("Cyfra 1: ", liczba[0])
+print("Cyfra 2: ", liczba[1])
+
+print("*"*50)
+
+print("Zadanie 2")
+liczba = input("Podaj trzycyfrowa liczbe: ")
+print("Cyfra 1: ", liczba[0])
+print("Cyfra 2: ", liczba[1])
+print("Cyfra 3: ", liczba[2])
+
+sumaliczb1 = liczba[0] + liczba[1] + liczba[2]
+print("Suma liczb" , sumaliczb1)
+
+print("*"*50)
+
+print("Zadanie 3")
+liczba = input("Podaj dwucyfrowa liczbe: ")
+print("Cyfry to : ", liczba[0],liczba[1])
+
+print("*"*50)
+
+print("Zadanie 4")
+tempcel = int(input("Podaj temp w celsjuszach: "))
+fahrenheit =   (tempcel * 9/5) + 32
+print("Temp w fahrenheitach wynosi: ", fahrenheit)
