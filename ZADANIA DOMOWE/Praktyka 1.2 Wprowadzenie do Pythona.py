17f80c923105e954c9fb09ab5424304c63ba2584
toybro <krzysiupoc@gmail.com>
2025-10-17 22:35:20 +0200

    Add files via upload

---
+++ ZADANIA DOMOWE/Praktyka 1.2 Wprowadzenie do Pythona.py
@@ -0,0 +1,46 @@
+from Projekt1.lekcja_8_metoda_zip import dluzsza_lista
+
+print("Zadanie 1")
+print("\t\tNothing")
+print("\t\t\twill work")
+print("\t\t\tunless you do")
+
+print("*"*50)
+
+print("Zadanie 2")
+print("\t\"Anyone who")
+print("\t\tstops")
+print("\t\t\tlearning is old,")
+print("\t\t\t\twhether at twenty or eighty")
+print("\t\t\t\t\tHenry Ford")
+
+print("*"*50)
+
+print("Zadanie 3")
+liczba1 = int(input("Wpisz liczbe1: "))
+liczba2 = int(input("Wpisz liczbe2: "))
+
+print(f"Podałeś liczby {liczba1} oraz {liczba2}")
+suma = liczba1 + liczba2
+print(f"Suma {suma}")
+iloczyn = liczba1 * liczba2
+print(f"Iloczyn {iloczyn}")
+roznica = liczba1 / liczba2
+print(f"Roznica {roznica}")
+
+print("*"*50)
+
+print("Zadanie 4")
+wartosc = int(input("Wpisz wartosc: "))
+procent = int(input("Wpisz procent: "))
+wartosc_procent = (wartosc * procent) / 100
+print(f"Rozwiązanie wynosi {wartosc_procent}")
+
+print("*"*50)
+
+print("Zadanie 5")
+szerokosc = int(input("Wpisz szerokosc: "))
+wysokosc = int(input("Wpisz wysokosc: "))
+pole_prostokata = szerokosc*wysokosc
+print(f"Pole prostokata {pole_prostokata}")
+
