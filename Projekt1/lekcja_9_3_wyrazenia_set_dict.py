4eebd0d53d892e4306400917728bffb26626242c
toybro <34341513+toybro@users.noreply.github.com>
2025-10-23 19:53:34 +0200

    Add files via upload

--- Projekt1/lekcja_9_3_wyrazenia_set_dict.py
+++ Projekt1/lekcja_9_3_wyrazenia_set_dict.py
@@ -9,3 +9,23 @@ print(wynik)
 ksiazka_tel = {"Policja": 997, "Pizzeria": 123456789, "Numer alarmowy": 112}
 nowy_slownik = {wartosc: klucz for klucz, wartosc in ksiazka_tel.items()}
 print(nowy_slownik)
+################################
+print("*"*50)
+ksiazka_tel = {"Policja": 997, "Pizzeria": 123456789, "Numer alarmowy": 112}
+nowy_slownik = {}
+
+for klucz, wartosc in ksiazka_tel.items():
+    nowy_slownik[wartosc] = klucz
+
+print(nowy_slownik)
+
+
+ksiazka_tel["Andrzej"] = 987654321
+
+
+lista_slow = ["ala", "ola", "alek", "mirek", "ela"]
+dlugosc_slow = {}
+slowa = set(lista_slow)
+for slowo in slowa:
+    dlugosc_slow[slowo] = len(slowo)
+print(dlugosc_slow)
